07a205c174044510025f4a8a578229c20487fc7f
Rahul <rahul.wa47@gmail.com>
2026-05-09 21:15:18 +0530

    added state to store characters and initialized useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import Cards from './components/Cards'
 
 const App = () => {
+  const [characters, setCharacters] = useState([])
+
+  useEffect (()=> {
+    console.log("effect once")
+  }, [])
+
   return (
     <Cards />
   )
